ebba0abc13f4e1cccb50a319fcd406b9790d1db0
elettrapancotti <elettra.pancotti@gmail.com>
2026-05-06 17:03:51 +0000

    correggo errore in quicksort2.h

--- Esercitazione5/quicksort2.h
+++ Esercitazione5/quicksort2.h
@@ -10,9 +10,7 @@ int partition2(std::vector<T>& A, int p, int r) {//partizionamento con pivot a s
     T x = A[r];//sceglie ultimo elemento come pivot
     int i = p-1;//indice dell'ultimo elemento <= pivot (inizialmente nessuno)
 
-    for (int j = p; j<r; j+=1){ //scorre il vettore da p a r-1
-
-    for (int j = p; j<r; j+=1){
+    for (int j = p; j<r; j+=1){  //scorre il vettore da p a r-1
         if (A[j]<x) { //confronta con il pivot
             i += 1;
             std::swap(A[i], A[j]); //espande la zona degli elementi<= pivot
@@ -20,6 +18,7 @@ int partition2(std::vector<T>& A, int p, int r) {//partizionamento con pivot a s
     }
     std::swap(A[i+1], A[r]); //mette il pivot al posto giusto
     return i + 1; //restituisce l'indice del pivot
+}
 
 template<typename T>
 void quicksort2(std::vector<T>& A, int p, int r) {
